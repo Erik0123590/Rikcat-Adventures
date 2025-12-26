a757bd567bff8dd8b0361110ca0f17fe4611e2a7
Erik0123590 <eriktop43@gmail.com>
2025-12-26 20:49:43 -0300

    Set up Firebase configuration and exports
    Initialized Firebase app and database connection.

--- firebase.js
+++ firebase.js
@@ -1,6 +1,17 @@
-// Multiplayer Online (base pronta)
-// Será ativado depois
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, set, onValue, onDisconnect } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-const onlineMode = false;
+const firebaseConfig = {
+  apiKey: "AIzaSyDCGDk08XGGYOTYnXchuDDrBS0emCm87P0",
+  authDomain: "rikcatonline.firebaseapp.com",
+  databaseURL: "https://rikcatonline-default-rtdb.firebaseio.com",
+  projectId: "rikcatonline",
+  storageBucket: "rikcatonline.firebasestorage.app",
+  messagingSenderId: "504285237002",
+  appId: "1:504285237002:web:9841ceb83ea0fe919674f3"
+};
 
-// Aqui entrará o Firebase futuramente
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, set, onValue, onDisconnect };
