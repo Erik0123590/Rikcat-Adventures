c2099de2c07cf8446ea3be051e244fd9c0cf3d8b
Erik0123590 <eriktop43@gmail.com>
2025-12-28 09:32:51 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  set,
-  onValue,
-  onDisconnect,
-  push,
-  remove
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, set, onValue, onDisconnect, push } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDCGDk08XGGYOTYnXchuDDrBS0emCm87P0",
   authDomain: "rikcatonline.firebaseapp.com",
   databaseURL: "https://rikcatonline-default-rtdb.firebaseio.com",
   projectId: "rikcatonline",
-  storageBucket: "rikcatonline.firebasestorage.app",
+  storageBucket: "rikcatonline.appspot.com",
   messagingSenderId: "504285237002",
   appId: "1:504285237002:web:9841ceb83ea0fe919674f3"
 };
@@ -22,12 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export {
-  db,
-  ref,
-  set,
-  onValue,
-  onDisconnect,
-  push,
-  remove
-};
+export { db, ref, set, onValue, onDisconnect, push };
