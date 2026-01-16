2de2fa2d55dd88fbe2c12a8c68e3cd3ad5b5ee0b
Erik0123590 <eriktop43@gmail.com>
2026-01-16 14:25:28 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,7 +2,7 @@ import { Config } from "./config.js";
 
 document.getElementById("btnADM").onclick = () => {
   const senha = prompt("Coloque a senha");
-  if (senha === "rikcat") {
+  if (senha === "RikcatADM!2025") {
     alert("Comandos de ADM ligados");
     Config.admEnabled = true;
     Config.fireEnabled = true;
